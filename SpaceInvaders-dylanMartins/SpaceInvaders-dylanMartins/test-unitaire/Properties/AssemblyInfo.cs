6826ab716c1eeda9593a4d4b4aebc53e121fdb83
Dylan Martins <89643025+Dedytes@users.noreply.github.com>
2022-10-12 16:30:05 +0200

    Update
    Création de la page de programmation,
    
    à faire : test unitaire

---
+++ SpaceInvaders-dylanMartins/SpaceInvaders-dylanMartins/test-unitaire/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test-unitaire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test-unitaire")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00b5e2f5-bf9f-47d5-8da0-60be2a4c5bd5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpaceInvaders-dylanMartins/SpaceInvaders-dylanMartins/test-unitaire/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pu17alr_Student;
+
+namespace test_unitaire
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Student student1 = new Student("Pete", "Cation", 1000);
+            // Ajout des notes pour cet étudiant
+            student1.AddSchoolMarks(4.5);
+            student1.AddSchoolMarks(5.5);
+            student1.AddSchoolMarks(5.0);
+            double avg;
+
+            //Act
+
+            avg = student1.SchoolMarksAverage();
+
+            //Assert
+            Assert.AreEqual(5.0, avg, "La moyenne doit être de 5.0");
+        }
+    }
+}
